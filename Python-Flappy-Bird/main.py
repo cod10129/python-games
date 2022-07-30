46c0af79fdd0d1930eba67b8c83c1b337b01f440
cod10129 <davidahuett10@gmail.com>
2022-07-29 20:44:39 -0500

    Added basic jump. Pipe WIP

--- Python-Flappy-Bird/main.py
+++ Python-Flappy-Bird/main.py
@@ -1,18 +1,36 @@
 # import main library(ies)
-import sys
+import sys, random
+
 # import pygame library (make sure to install it on your computer)
 import pygame
 pygame.init()
 
 # initialize pygame objects
 screen = pygame.display.set_mode((500, 500))
-player = pygame.image.load("./icons/bird.jpg")
+player = pygame.image.load("./Python-Flappy-Bird/icons/bird.jpg")
 playerRect = player.get_rect()
-playerRect = (50, 200)
+playerRect = [50, 200]
+playerSpeed = 0
 
 # Pipe Class
 class Pipe:
-    pass
+    def __init__(self, x):
+        self.x = x
+        self.width = 75
+        self.holeY = random.randint(75, 425)
+        self.gap = 100 # TODO This should become smaller the farther the inital X is
+        
+    
+    def draw(self):
+        pass
+        # TODO 
+        
+
+# Initialize Pipe Objects
+
+
+# Other Initalization
+playing = False
 
 # Frame Loop
 while True:
@@ -21,6 +39,20 @@ while True:
         # If event = QUIT, close out
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN: # Check for keypress
+            # Jump Code
+            if event.key == pygame.K_SPACE: # Check if keypress was space
+                playerSpeed = 0.1   # TODO balancing w/ gravity (force set is bc of feeling of playing flappy bird)
+            # Start Game
+            elif event.key == pygame.K_1:
+                playing = True
+            
+    
+    # Player Events
+    
+    if playing == True:
+        playerSpeed -= 0.0001 # TODO gravity (balancing)
+    playerRect[1] -= playerSpeed # Change player Y by ySpeed (the minus equals is bc the higher Y vals are farther down, and I wanted to make ySpeed at least *seem* intuitive)
     
     # Screen Update
     screen.fill((255, 255, 255))
